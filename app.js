e62451c94c29f837b1f888ee7aec5b36acd715a1
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-04-09 14:43:10 -0500

    Coding complete

--- app.js
+++ app.js
@@ -1,36 +1,92 @@
-function init() {
+// Drop down button has sample ID data
+// Pull in the sample.json data. 
+let data = d3.json("samples.json").then(data => {
+
+    // Pull the data from the json
+    const samples = data.samples;
+    const metadata = data.metadata;
+    const names = data.names;
+
+    // ID names listed into the drop down
+    let dropDown = d3.select('#selDataset')
+    names.forEach(name => {
+        dropDown.append('option').text(name).property('value', name);
+    });
+});
+
+// Function to pull data based on drop down selection
+function optionChanged(selection) {
+    demographicInfo(selection);
+    buildCharts(selection)
+};
+
+// Function to pull the demographic metadata for sample ID
+function demographicInfo(id) {
     d3.json("samples.json").then(data => {
-        console.log("read samples");
-        console.log(data);
-        console.log("reading the detail in the json");
-        console.log(data.metadata[0]["id"]);
-        console.log("trying to get an array of id's");
+        // Define a variable to store the metadata form the json
+        let metadata = data.metadata;
+        // Define a variable for the Demographic Info Box
+        let demoInfoBox = d3.select('#sample-metadata');
+        // Clear the demographic metadata (will continue to append if not included)
+        demoInfoBox.html('');
+        // Filter the metadata to to pull out only the data related to the sample ID
+        let filteredData = metadata.filter(sampleName => sampleName.id == id)[0];
+        // Append the filtered data into the HTML code, iterating through the keys of the dictionary
+        // Object.entries is a shortcut to iterate through key / value pairs in a dictionary
+        Object.entries(filteredData).forEach(([key, value]) => {
+            demoInfoBox.append("h5").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+};
 
-        let options = [];
-       
+// Function to plot the OTU's for a given sample ID
+function buildCharts(id) {
+    d3.json("samples.json").then(data => {
+        let filteredData = data.samples.filter(SampleId => SampleId.id == id)[0];
+        let sampleValues = filteredData.sample_values;
+        let sampleIds = filteredData.otu_ids;
+        let sampleLabels = filteredData.otu_labels;
+        
+        // Build the horizontal bar chart
+        // Setup the variables and chart parameters
+        let trace1 = {
+            x: sampleValues.slice(0,10).reverse(),
+            y: sampleIds.slice(0,10).map(otu_id => `OTU ${otu_id}`).reverse(),
+            text: sampleLabels.slice(0,10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        };
+        let barChartVariables = [trace1];
+        // Setup the layout of the chart
+        let layout1 = {
+            title: `Top 10 OTU's in Subject ID ${id}.`
+        };
+        // Plot the chart
+        Plotly.newPlot("bar", barChartVariables, layout1);
 
-        for (let i = 0; i < data.metadata.length; i++) {
-            options.push(data.metadata[i]["id"]);
-            
+        //Build the bubble chart
+        // Setup the variables and chart parameters
+        let trace2 = {
+            x: sampleIds,
+            y: sampleValues,
+            text: sampleLabels,
+            mode: 'markers',
+            marker: {
+                size: sampleValues,
+                color: sampleIds
+            }
         };
 
-        console.log("checking to see if id prints")
-        console.log(options);
+        let bubbleChartVariables = [trace2];
 
-        let select = document.getElementById("selDataset");
-        for (let i = 0; i < options.length; i++) {
-            let opt = options[i];
-            let el = document.createElement("option");
-            el.text = opt;
-            el.value = opt;
+        let layout2 = {
+            title: `Values for Subject ID ${id}`,
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
 
-            select.add(el);
-        }      
-
+        Plotly.newPlot('bubble', bubbleChartVariables, layout2);
+        
     });
-}
-function optionChanged(value) {
-    console.log(value);
-}
-
-init();
+};
